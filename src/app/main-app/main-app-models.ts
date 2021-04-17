aba0947541757133193e8a23c92afa3f1b824c39
kalana <kalanasuraweera@gmail.com>
2021-04-17 17:47:31 +0530

    removed reasons module

--- src/app/main-app/main-app-models.ts
+++ src/app/main-app/main-app-models.ts
@@ -4,7 +4,6 @@ export enum MODULE_TYPE {
   TEAM_MEMBERS = 'team_members',
   MODULES = 'modules',
   ROOT_CAUSES = 'root_causes',
-  REASONS = 'reasons',
   TEAM_MEMBER_ALLOCATIONS = 'tm_alloc',
   MODULE_TEAM_ALLOCATIONS = 'mt_alloc',
   GENERATE_REPORT = 'gen_report'
